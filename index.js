5cc98c7e219ed686a67a4d12cddd2e1f07a140fa
zena abd alkarem alhello <zena34269@gmail.com>
2020-08-09 19:48:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
 
-Function writeCards(names,birthday){
-  const newarray=[];
-  for(let i=0;i<names.length;i++){
-    newarray[i]=`Thank you, + ${names[i]} +, for the wonderful + ${ birthday}+ gift!`;
-  }
-  return newarray
-}
-
-writeCards(["Ada", "Brendan", "Ali"], "surprise");
-countDown(10);
